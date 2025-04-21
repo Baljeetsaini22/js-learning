25b0b195b1e25863be0e1b192c17b2487c00f968
Baljeetsaini22 <baljeetsaini7440@gmail.com>
2025-04-21 15:01:28 +0530

    change program

--- practices in js/DomManipluation.js
+++ practices in js/DomManipluation.js
@@ -16,3 +16,115 @@ button.addEventListener("click", function() {
 button.addEventListener('click', function(){
     
 })
+
+
+// !======================| v| -----------------------
+
+
+// ? function return largest three number
+// function globalfunc(){
+//         console.log(`${this[id]},${this.name},${this.meal}`)
+// }
+
+
+// const id = Symbol()
+// const onj = {
+//   [id]: 1,
+//   name : 'tigger',
+//   age: 10,
+//   meal : "non-veg",
+// };
+
+// const obj2 = {
+//     [id]: 2,
+//     name : 'tigger',
+//     age: 10,
+//     meal : "non-veg",
+//   };
+// globalfunc.call(onj)
+// globalfunc.call(obj2)
+
+
+
+// onj.bio()
+// onj.hunting()
+
+
+// factory function
+ 
+const server = [] 
+function bioDataFactory(id, name, age, bloodGroup, address, city, state, phoneNumber, telephoneNumber, pincode, country) {
+    return {
+        id,
+        name,
+        age,
+        bloodGroup,
+        address,
+        city,
+        state,
+        phoneNumber,
+        telephoneNumber,
+        pincode,
+        country
+    };
+}
+
+
+
+const person = bioDataFactory(
+    1, 
+    "Alice", 
+    30, 
+    "O+", 
+    "123 Street Name", 
+    "New York", 
+    "NY", 
+    "1234567890", 
+    "0987654321", 
+    "10001", 
+    "USA"
+);
+
+server.push(person)
+// console.log(server)
+
+/** __proto__ */
+// to create chaning between to objects 
+
+const onb1 = {
+    a : 32,
+    b: 34
+}
+
+const obj2 = onb1
+
+obj2.c = 4
+// console.log(obj2.a)
+
+
+// __proto__ [[prototype]]
+// prototype [[prototype]]
+
+
+
+/** prototype  */
+
+function createUsr(fstName, lstName, age, email, address) {
+    const usr = Object.create(createUsr.prototype); // give a empty object with __proto__ refrence
+    // const usr = {};
+    usr.fstName = fstName;
+    usr.lstName = lstName;
+    usr.age = age;
+    usr.email = email;
+    usr.address = address;
+    return usr;
+};
+
+const usr1 = createUsr("gamer", "pro", 11, "gam@mail.to", "#12 st.3434");
+createUsr.prototype.about = function () { return `${this.fstName} ${this.age}` };
+
+createUsr.prototype.sendPost = function(){
+    return `${this.email} ${this.address}`
+}
+
+console.log(usr1.sendPost())
