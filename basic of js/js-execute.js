313745d4a084b4f4ed904732b4e2dda0c6a5b213
Baljeetsaini22 <baljeetsaini7440@gmail.com>
2025-04-21 10:40:11 +0530

    update line

--- basic of js/js-execute.js
+++ basic of js/js-execute.js
@@ -1,8 +1,23 @@
-////?=================Execution context================= 
-//1. Global Execution Context
+////?=================JavaScript Execution context================= 
+//1. Global Execution Context (Global Enviorment) locate in (this)
 //2. Function Execution Context
 //3. Eval Execution Context
-// {} -> Memory Creation Phase
+// i. {} -> Memory Creation Phase 
+// -> collect the variable and store himself 
+// val1 -> undefined
+// val2 -> undefined
+// addNum -> defination
+// result 1 -> undefined
+// resutl2 -> undefined
+
+// ii. Execution Phase
+// val1 <-  10
+// val2 <-  5
+// addNum -> newVariable, Enviorment + execution thread
+// -> memory phase -> val1 -> undefined, val2 -> undefined
+// total -> undefined
+// -> Execution context -> num1 -> 10, num2 -> 5, total -> 15 
+// After work Comptlete Execution phase delete and repeat
 
 // let val1 = 10;
 // let val2 = 5;
@@ -14,3 +29,4 @@
 // let result2 = addNum(10,  2)
 
 
+//! ===========Call stack=======================
